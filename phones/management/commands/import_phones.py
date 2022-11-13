28e6fd5b611096232a6b5e3b6462029c55c875ef
FrostPy <ua_frost@mail.ru>
2022-11-13 19:39:17 +0300

    first_commit

---
+++ phones/management/commands/import_phones.py
@@ -0,0 +1,24 @@
+import csv
+from django.template.defaultfilters import slugify
+from django.core.management.base import BaseCommand
+from phones.models import Phone
+
+
+class Command(BaseCommand):
+    def add_arguments(self, parser):
+        pass
+
+    def handle(self, *args, **options):
+        with open('phones.csv', 'r') as f:
+            phones = csv.reader(f, delimiter=';')
+            next(phones)
+
+            for phone in phones:
+                new_phone = Phone.objects.create(
+                       id=int(phone[0]),
+                       name=phone[1],
+                       image=phone[2],
+                       price=int(phone[3]),
+                       release_date=phone[4],
+                       lte_exists=phone[5],
+                       slug=slugify(phone[1]))
---
+++ phones/models.py
@@ -0,0 +1,20 @@
+from django.db import models
+
+
+class Phone(models.Model):
+    id = models.IntegerField(primary_key=True)
+    name = models.CharField(max_length=50)
+    image = models.ImageField()
+    price = models.FloatField()
+    release_date = models.DateTimeField()
+    lte_exists = models.BooleanField()
+    slug = models.SlugField()
+
+    def __str__(self):
+        return f"{self.id};" \
+               f" {self.name};" \
+               f" {self.price};" \
+               f" {self.image};" \
+               f" {self.release_date};" \
+               f" {self.lte_exists};" \
+               f" {self.slug}"
